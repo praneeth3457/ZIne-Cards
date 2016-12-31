2dcc897f752a1614562f37d8694401fecd03e3ec
Praneeth Raj Reddy Veerlapally <praneethrajreddyveerlapally@Praneeths-MacBook-Pro.local>
2016-12-31 00:31:33 -0800

    sadadad

--- src/app/stats/stats.js
+++ src/app/stats/stats.js
@@ -2,9 +2,14 @@ var app = angular.module('zine-cards');
 app.controller('statsController', function(statsService) {
   var stat = this;
   statsService.stats().then(function(res){
-    console.log(res);
     if(res.data.message == 'Success') {
       stat.data = res.data.stats;
+
+      //Order by average
+      sortByKey(stat.data, 'avg');
+      function sortByKey(array, key) {
+        stat.avgData = array.sort(function(a,b) { return b[key] - a[key];});
+      }
     }
   }, function(error) {
 
